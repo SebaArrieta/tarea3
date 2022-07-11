e6e8f3490cefa32256f7616d272011e87a29bca8
SebaArrieta <102710333+SebaArrieta@users.noreply.github.com>
2022-07-10 22:57:24 -0400

    Update minHeap.h

--- minHeap.h
+++ minHeap.h
@@ -20,21 +20,42 @@ class tColaP{
         size = 0;
     }
 
-    void clean(){
-        size = 0;
-    }
-
     void view(){
         for(int i = 0; i < size-1; i++){
             cout<<i+1<<". "<<root[i].consultas<<endl;
         }
     }
+    
+/*****
+* void swap
+******
+* esta funcion intercambia elementos en el minheap cuando es necesario durante la insercion o el borrado de algun componente.
+******
+* Input:
+* int pos1: poscion del primer elemento a intercambiar.
+  int pos2:  poscion del segundo elemento a intercambiar.
+* .......
+******
+* Returns:
+*****/
 
     void swap(int pos1, int pos2){
         nodo aux = root[pos1];
         root[pos1] = root[pos2];
         root[pos2] = aux;
     }
+  /*****
+* void insertColaP
+******
+* Esta funcion inserta los elementos al minheap y asi buscar la minima cantidad de consultas por elemento cuando es necesario.
+******
+* Input:
+* nodo n: termino a insertar al minheap acorde a la cantidad de busquedas.
+* .......
+******
+* Returns:
+
+*****/
 
     void insertColaP(nodo n){
         size+=1;
@@ -48,10 +69,37 @@ class tColaP{
             }   
         }
     }
+    
+  /*****
+* int findMin
+******
+* Esta funcion entrega la posicion en el hashing del elemento con menos cantidad de busquedas.
+******
+* Input:
+* 
+* .......
+******
+* Returns:
+*
+*****/
 
     int findMin(){
         return root[0].key;
     }
+   
+ /*****
+* void hundir
+******
+* Esta funcion recursiva hunde los elementos cuando ocurre el intercambio durante el proceso de eliminación para mantener el termino con la minima cantidad de consultas en 
+la posicion 1 del minheap
+******
+* Input:
+* int pos: inicialmente es 1 y luego va cambiando dependiendo de la nueva posicion establecida para el elemento.
+* .......
+******
+* Returns:
+*
+*****/
 
     void hundir(int pos){
         int izq = -1;
@@ -83,6 +131,19 @@ class tColaP{
                 hundir(pos*2);
         }
     }
+    
+    /*****
+* void removeMin
+******
+* Esta funcion elimina el termino con la minima cantidad de consultas y lo intercambia con siguiente elemento en la lista de minimos necesarios para la limpieza del hashing.
+******
+* Input:
+* 
+* .......
+******
+* Returns:
+*
+*****/
 
     void removeMin(){
         swap(0, size-1);
